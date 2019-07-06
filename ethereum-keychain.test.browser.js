86b44917931ae941f69292a53136d9db4d80b183
makevoid <makevoid@gmail.com>
2019-07-06 03:18:53 +0100

    file renaming

---
+++ ethereum-keychain.test.browser.js
@@ -0,0 +1,39 @@
+// NOTE:
+// ths test is configured to use real xDAIs
+// #lazyprogrammer #cheapalts
+
+const { Keychain } = require('./ethereum-keychain')
+
+// new Keychain
+
+// const NODE_JS = true
+const NODE_JS = false
+
+let wallet
+
+// nodejs
+if (NODE_JS) {
+  const { readFileSync } = require('fs')
+  const pvtKey = readFileSync('./.private-key.txt').toString().trim()
+  wallet = new Keychain({ store: { "__ethereum-keychain_": pvtKey } })
+} else {
+  wallet = new Keychain({ store: localStorage })
+}
+// const wallet = new Keychain({ store: {} })
+wallet.info()
+
+;(async () => {
+  try {
+    await wallet.netInfo()
+    await wallet.selfTXTest()
+    await wallet.netInfo()
+    const process = require('process')
+    process.exit()
+  } catch (err) {
+    console.log("Caught async error")
+    console.error(err)
+    console.error(err.stack)
+  }
+})()
+
+// new Keychain({ store: localStorage }) // browser
